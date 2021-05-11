5f5dd2285b956a6eb8ca17be036f8ed3dec259e4
RoshRoslin <RoshRoslin@gmail.com>
2021-05-11 12:41:40 -0700

    Updates

---
+++ precache-manifest.5307ba20472c82702dd6c7e9ad0c5e05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae602a67722a8cea7d8ff51e9cd508f6",
+    "url": "/react-navbar-project/index.html"
+  },
+  {
+    "revision": "0fe0790f547d05809d63",
+    "url": "/react-navbar-project/static/css/main.beaf029b.chunk.css"
+  },
+  {
+    "revision": "7c7331f0131e2cf50072",
+    "url": "/react-navbar-project/static/js/2.af817d26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-navbar-project/static/js/2.af817d26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe0790f547d05809d63",
+    "url": "/react-navbar-project/static/js/main.98e5b217.chunk.js"
+  },
+  {
+    "revision": "ad5c53802d83291362f9",
+    "url": "/react-navbar-project/static/js/runtime-main.0c843766.js"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/react-navbar-project/static/media/logo.05a3e30f.svg"
+  }
+]);
